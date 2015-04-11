a6c5f06fc4518b8d2852f678172a9658583b3cc2
Hamid FzM <hamidfzm@gmail.com>
2015-04-12 00:51:59 +0430

    add bullet object

---
+++ include/Bullet.h
@@ -0,0 +1,48 @@
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string.h>
+
+// Constants
+#include "Defines.h"
+
+// Game Tools
+#include "Utils.h"
+#include "Timer.h"
+
+// Game
+#include "Resources.h"
+
+class Bullet
+{
+public:
+	Bullet();
+	Bullet(Resources *, const int, const int, const int, const int);
+	float getX();
+	float getY();
+	float getVX();
+	float getVY();
+	float getWidth();
+	float getHeight();
+	double getAngle();
+	
+	void setX(const float);
+	void setY(const float);
+	void setVX(const float);
+	void setVY(const float);
+	void setAngle(const double);
+	void update();
+	void draw();
+
+private:
+	Resources *res;
+	static SDL_Rect body[];
+	int kind;
+	int direction;
+	float x, y, vx, vy;
+	double angle;
+};
+
+#endif // BULLET_H
---
+++ src/Bullet.cpp
@@ -0,0 +1,106 @@
+#include "Bullet.h"
+
+SDL_Rect Bullet::body[] = {	{84, 481, 12, 26},  // silver
+							{48, 481, 12, 26},  // beige
+							{120, 481, 12, 26},  // red
+							{0, 481, 12, 26},  // blue
+							{96, 481, 12, 26},  // yellow
+							{24, 481, 12, 26}};  // green
+
+Bullet::Bullet()
+{
+}
+
+Bullet::Bullet(Resources *res, const int kind, const int direction, const int x, const int  y) :kind(kind), res(res), direction(direction), x(x), y(y)
+{
+	if (direction & MOVE_UP){
+		angle = 0;
+	} else {
+		angle = 180;
+	}
+	
+	setVX(1);
+	setVY(1);
+}
+
+void Bullet::setX(const float x)
+{
+	this->x = x;
+}
+
+void Bullet::setY(const float y)
+{
+	this->y = y;
+}
+
+void Bullet::setVX(const float vx)
+{
+	
+	this->vx = vx;
+}
+
+void Bullet::setVY(const float vy)
+{
+	this->vy = vy;
+}
+
+void Bullet::setAngle(const double angle)
+{
+	this->angle = angle;
+}
+
+float Bullet::getX()
+{
+	return x;
+}
+
+float Bullet::getY()
+{
+	return y;
+}
+
+float Bullet::getVX()
+{
+	return vx;
+}
+
+float Bullet::getVY()
+{
+	return vy;
+}
+
+float Bullet::getWidth()
+{
+	return body[kind].w;
+}
+
+float Bullet::getHeight()
+{
+	return body[kind].h;
+}
+
+double Bullet::getAngle()
+{
+	return angle;
+}
+
+void Bullet::update()
+{
+	if (direction & MOVE_UP){
+		y -= (vy / BASE_TIME) * (res->timeDelta);
+	} else if (direction & MOVE_DOWN){
+		y += (vy / BASE_TIME) * (res->timeDelta);
+	}
+
+	if (direction & MOVE_LEFT){
+		x -= (vx / BASE_TIME) * (res->timeDelta);
+	} else if (direction & MOVE_RIGHT){
+		x += (vx / BASE_TIME) * (res->timeDelta);
+	}
+}
+
+
+void Bullet::draw()
+{
+	renderTexture(res->sprites_txt, res->renderer, x - body[kind].w/2, y - body[kind].h/2, angle, &body[kind]);
+}
